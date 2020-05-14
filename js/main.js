5a15a3f33a2eee867f7ddd6a812a086d5c79bf97
sean <seangeekpro@gmail.com>
2020-05-14 06:47:11 +0200

    Menu fixed.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".menu-btn").on("click", () => {
     $(".main-menu").toggleClass("show");
+    setTimeout(() => {
+      $(".main-menu").toggleClass("opacity1");
+    }, 500);
   });
 });

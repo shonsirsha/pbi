6346a519b8a01d3d23d361c77c4b63011e621a23
sean <seangeekpro@gmail.com>
2020-05-15 13:32:49 +0200

    added corresponding page links.

--- js/main.js
+++ js/main.js
@@ -20,4 +20,28 @@ $(document).ready(function () {
       }
     }
   });
+
+  $(".box1").on("click", () => {
+    location.href = "./about-us.html";
+  });
+
+  $(".box2").on("click", () => {
+    location.href = "./courses.html";
+  });
+
+  $(".box3").on("click", () => {
+    location.href = "./activities.html";
+  });
+
+  $(".box4").on("click", () => {
+    location.href = "./accommodations.html";
+  });
+
+  $(".box5").on("click", () => {
+    location.href = "./yogyakarta.html";
+  });
+
+  $(".box6").on("click", () => {
+    location.href = "./students-reviews.html";
+  });
 });

9cf487f4748a6fb3bf7896abeae1bf7d80674fe5
sean <seangeekpro@gmail.com>
2020-06-06 13:35:10 +0200

    menu creator script init

---
+++ js/test.js
@@ -0,0 +1,34 @@
+let jsonMenu = [
+  {
+    sectionName: "Personal Details",
+    content: { fullName: "text", nationality: "text", sex: ["female", "male"] },
+  },
+  {
+    sectionName: "Term Of Payment",
+    content: {
+      paidBy: ["individually", "organization"],
+      paidUponArrival: ["cash", "credit", "bank transfer"],
+    },
+  },
+];
+
+jsonMenu.forEach((menuSection, i) => {
+  console.log("<section class='" + i + "'>" + menuSection.sectionName + "");
+  for (var inputName in menuSection.content) {
+    if (menuSection.content.hasOwnProperty(inputName)) {
+      if (Array.isArray(menuSection.content[inputName])) {
+        console.log("<option>");
+        menuSection.content[inputName].forEach((x) => {
+          console.log("<section>" + x + "</section>");
+        });
+        console.log("</option>");
+      } else {
+        console.log(
+          inputName + " input of this: " + menuSection.content[inputName]
+        );
+      }
+    }
+  }
+  console.log("</section>");
+  console.log("----------------------");
+});

d53890c6e96aeb32058d0bc061b1ba37fe6c4791
sean <seangeekpro@gmail.com>
2020-06-08 17:29:37 +0700

    multistep working with jquery

--- js/test.js
+++ js/test.js
@@ -11,102 +11,107 @@ let jsonForm = [
     },
   },
 ];
-const showCurrentPage = (renderedPageIndex, currentPage, sectionName) => {
-  let wrapper = document.getElementById("wrapper");
+
+const showCurrentPage = (renderedPageIndex, currentPage) => {
+  let wrapper = $("#wrapper");
 
   //shows current page/section and hides the other
   if (renderedPageIndex === currentPage) {
-    wrapper.innerHTML +=
-      "<section class='grid-2' id='multiStep" + renderedPageIndex + "'>";
+    wrapper.append(
+      "<section class='grid-2' id='multiStep" + renderedPageIndex + "'>"
+    );
   } else {
-    wrapper.innerHTML +=
-      "<section class='grid-2' class='hidden' id='multiStep" +
-      renderedPageIndex +
-      "'>";
+    wrapper.append(
+      "<section class='grid-2 hidden' id='multiStep" + renderedPageIndex + "'>"
+    );
   }
 };
 const btnShower = (page, lastPage) => {
-  let prevBtn = document.getElementById("prevBtn");
-  let nextBtn = document.getElementById("nextBtn");
-
   if (page === 0) {
     // first page
-    prevBtn.className = "hidden";
-    nextBtn.className = "shown";
-    nextBtn.innerHTML = "next";
+    $("#prevBtn").removeClass("shown");
+    $("#prevBtn").addClass("hidden");
+
+    $("nextBtn").removeClass("hidden");
+    $("#nextBtn").addClass("shown");
+
+    $("#nextBtn").html("Next");
   }
   if (page >= 1 && page < lastPage - 1) {
-    prevBtn.className = "shown";
-    nextBtn.className = "shown";
+    $("#prevBtn").addClass("shown");
+    $("#nextBtn").addClass("shown");
   }
 
   if (page === lastPage - 1) {
-    prevBtn.className = "shown";
-    nextBtn.className = "shown";
-    nextBtn.innerHTML = "send";
+    $("#prevBtn").addClass("shown");
+    $("#nextBtn").addClass("shown");
+    $("#nextBtn").html("Send");
   }
+
+  $("#sectionTitle").html("<h3>" + jsonForm[page].sectionName + "</h3>");
 };
 const createForms = (json) => {
-  let prevBtn = document.getElementById("prevBtn");
-  let nextBtn = document.getElementById("nextBtn");
   let currentPage = 0;
   let lastPage = json.length;
   btnShower(currentPage, lastPage);
 
-  nextBtn.addEventListener("click", () => {
+  $("#nextBtn").on("click", () => {
     if (currentPage === lastPage - 1) {
       // send
       alert("send");
     }
     currentPage++;
-    let nextPageSection = document.getElementById("multiStep" + currentPage);
-    let currentPageSection = document.getElementById(
-      "multiStep" + (currentPage - 1)
-    );
-    currentPageSection.className = "hidden";
-    nextPageSection.className = "shown";
+    let nextPageSection = $("#multiStep" + currentPage);
+    let currentPageSection = $("#multiStep" + (currentPage - 1));
+    currentPageSection.removeClass("shown");
+    currentPageSection.addClass("hidden");
+
+    nextPageSection.removeClass("hidden");
+    nextPageSection.addClass("shown");
     btnShower(currentPage, lastPage);
   });
 
-  prevBtn.addEventListener("click", () => {
+  $("#prevBtn").on("click", () => {
     currentPage--;
-    let prevPageSection = document.getElementById("multiStep" + currentPage);
-    let currentPageSection = document.getElementById(
-      "multiStep" + (currentPage + 1)
-    );
-    currentPageSection.className = "hidden";
-    prevPageSection.className = "shown";
+    let prevPageSection = $("#multiStep" + currentPage);
+    let currentPageSection = $("#multiStep" + (currentPage + 1));
+    currentPageSection.removeClass("shown");
+    currentPageSection.addClass("hidden");
+
+    prevPageSection.removeClass("hidden");
+    prevPageSection.addClass("shown");
     btnShower(currentPage, lastPage);
   });
 
   json.forEach((menuSection, i) => {
-    showCurrentPage(i, currentPage, menuSection.sectionName);
+    showCurrentPage(i, currentPage);
 
     for (var inputName in menuSection.content) {
       // loops through the 'content' obj
 
       if (menuSection.content.hasOwnProperty(inputName)) {
-        let pageSection = document.getElementById("multiStep" + i);
+        let pageSection = $("#multiStep" + i);
 
         if (Array.isArray(menuSection.content[inputName])) {
-          pageSection.innerHTML += "<select id='" + inputName + "'>";
-          let inputSelect = document.getElementById(inputName);
+          pageSection.append("<select id='" + inputName + "'>");
+          let inputSelect = $("#" + inputName);
           // if the 'content' value for this particular key is an array, then create a select input with a unique id.
 
           menuSection.content[inputName].forEach((x) => {
-            inputSelect.innerHTML += "<option>" + x + "</option>";
+            inputSelect.append("<option>" + x + "</option>");
           });
           //populate options (which are the content of the array) inside the select input created above.
         } else {
           // if it's not an array, then it's gonna be a normal input (text/email/phone nr,etc for now) -- no check for radios yet.
-          pageSection.innerHTML +=
+          pageSection.append(
             "<input class='form-input' type='" +
-            menuSection.content[inputName] +
-            "' name='" +
-            inputName +
-            "' placeholder='" +
-            inputName +
-            "'/>";
+              menuSection.content[inputName] +
+              "' name='" +
+              inputName +
+              "' placeholder='" +
+              inputName +
+              "'/>"
+          );
         }
       }
     }
